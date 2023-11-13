5a91e3170aa4f450b754180e860c06df7af3e50d
ww112543 <ww112543@github.com>
2023-11-13 18:15:58 +0800

    Set block size to 64MiB

--- src/video_core/vulkan_common/vma.h
+++ src/video_core/vulkan_common/vma.h
@@ -8,4 +8,6 @@
 #define VMA_STATIC_VULKAN_FUNCTIONS 0
 #define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
 
+#define VMA_DEFAULT_LARGE_HEAP_BLOCK_SIZE (64ull * 1024 * 1024) // 64MiB
+
 #include <vk_mem_alloc.h>
